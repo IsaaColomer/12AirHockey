8667983465e02390c498861253b55c4642e77353
IsaaColomer <60975028+IsaaColomer@users.noreply.github.com>
2022-11-07 11:10:19 +0100

    added the power ups code but duesnt work because of scale problem

--- My project/Assets/Code/Game/playerScript.cs
+++ My project/Assets/Code/Game/playerScript.cs
@@ -5,23 +5,83 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    [SerializeField] private UnityEngine.Vector3 initScale;
     [SerializeField] private Rigidbody rb;
     public LayerMask ignore;
     [SerializeField] private Camera cam;
     private int powerType;
-    // Start is called before the first frame update
+    public float applyPowerTime = 3;
+    private float initApplyPowerTime;
+    private UnityEngine.Vector3 reducedScale;
+    private UnityEngine.Vector3 growedScale;
+    private bool startApply = false;
+    //gran petit
+    //congelar
+    //el disc va mes rapid
     void Start()
     {
         rb = gameObject.GetComponentInChildren<Rigidbody>();
         cam = GetComponent<Camera>();
+        initScale = gameObject.GetComponentInChildren<Transform>().localScale;
+        initApplyPowerTime = applyPowerTime;
+        reducedScale = initScale / 1.5f;
+        growedScale = initScale * 1.5f;
     }
 
     public void GetType(int type)
     {
         powerType = type;
+        startApply = true;
         Debug.Log("Player power: " + powerType);
     }
-
+    void ApplyPowerUp(int type)
+    {
+        switch(type)
+        {
+            case 0:
+                if(applyPowerTime > 0)
+                {
+                    applyPowerTime -= Time.deltaTime;
+                    gameObject.GetComponentInChildren<Transform>().localScale = reducedScale;
+                    Debug.Log(applyPowerTime);
+                }
+                else
+                {
+                    applyPowerTime = initApplyPowerTime;
+                    gameObject.GetComponentInChildren<Transform>().localScale = initScale;
+                    startApply = false;
+                }
+                break;
+            case 1:
+                if (applyPowerTime > 0)
+                {
+                    applyPowerTime -= Time.deltaTime;
+                    gameObject.GetComponentInChildren<Transform>().localScale = growedScale;
+                    Debug.Log(applyPowerTime);
+                }
+                else
+                {
+                    applyPowerTime = initApplyPowerTime;
+                    gameObject.GetComponentInChildren<Transform>().localScale = initScale;
+                    startApply = false;
+                }
+                break;
+            case 2:
+                if (applyPowerTime > 0)
+                {
+                    applyPowerTime -= Time.deltaTime;
+                    gameObject.GetComponentInChildren<Transform>().localScale = growedScale;
+                    Debug.Log(applyPowerTime);
+                }
+                else
+                {
+                    applyPowerTime = initApplyPowerTime;
+                    gameObject.GetComponentInChildren<Transform>().localScale = initScale;
+                    startApply = false;
+                }
+                break;
+        }
+    }
     // Update is called once per frame
     void Update()
     {
@@ -39,6 +99,11 @@ public class PlayerScript : MonoBehaviour
                 rb.velocity = dir * 10f;
                 Debug.DrawRay(transform.position, mousePos-transform.position, Color.green);
             }
+        }
+
+        if(startApply)
+        {
+            ApplyPowerUp(powerType);
         }
     }
 }
